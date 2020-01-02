e5f787a496e1328859d92f956dbd97cb7c07e912
eskri969 <krick96@gmail.com>
2020-01-02 17:23:00 +0100

    sending avg to thingsboard working

---
+++ thingsboard-python/hive.py
@@ -0,0 +1,80 @@
+
+#https://pypi.org/project/paho-mqtt/
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+import time
+import sys
+import random
+
+
+# Don't forget to change the variables for the MQTT broker!
+topic_pub1="hive/1/telemetry"
+topic_pub2="hive/2/telemetry"
+topic_pub3="hive/3/telemetry"
+
+mqtt_broker_ip = "127.0.0.1"
+client = mqtt.Client(protocol=mqtt.MQTTv311, transport="tcp")
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    #client.subscribe(topic_pub)
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print("NEW MSG -> "+msg.topic+" "+str(msg.payload.decode('UTF-8')))
+
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        print ("Unexpected MQTT disconnection. Will auto-reconnect")
+
+if __name__ == "__main__":
+    try:
+        client.username_pw_set("IU8rjHe8MCyu0A0oqk7S", password=None)
+        client.connect(mqtt_broker_ip, 1883, 60)
+        client.on_connect = on_connect
+        client.on_message = on_message
+        client.on_disconnect = on_disconnect
+        client.loop_start()
+        i=0
+        temperatureIn=0
+        temperatureOut=0
+        humidityIn=0
+        humidityOut=0
+        weigth0=0
+        weigth1=0
+        weigth2=0
+        X=0
+        Y=0
+        Z=0
+        C02=0
+        print("init")
+        while True:
+            i+=1
+            temperatureIn+=1
+            temperatureOut+=1
+            humidityIn+=1
+            humidityOut+=1
+            weigth0+=1
+            weigth1+=1
+            weigth2+=1
+            X+=1
+            Y+=1
+            Z+=1
+            C02+=1
+            msg="{\"temperatureIn\":"+str(temperatureIn)+",\"temperatureOut\":"+str(temperatureIn)+",\"humidityIn\":"+str(temperatureIn)+",\"humidityOut\":"+str(temperatureIn)+",\"weigth0\":"+str(temperatureIn)+",\"weigth1\":"+str(temperatureIn)+",\"weigth2\":"+str(temperatureIn)+",\"X\":"+str(temperatureIn)+",\"Y\":"+str(temperatureIn)+",\"Z\":"+str(temperatureIn)+",\"C02\":"+str(temperatureIn)+"}"
+            print("send"+str(i))
+            client.publish(topic_pub1, msg)
+            client.publish(topic_pub2, msg)
+            client.publish(topic_pub3, msg)
+            time.sleep(15)
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        print(e)
+    finally:
+        client.loop_stop(force=False)
+        sys.exit(0)
--- thingsboard-python/main.py
+++ thingsboard-python/main.py
@@ -10,19 +10,19 @@ from tb_device_mqtt import TBDeviceMqttClient, TBPublishInfo
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 
-logging.basicConfig(level=logging.DEBUG)
-#logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 
 ## Thinsgboard vad def
 server_address = "srv-iot.diatel.upm.es"
 values_TB_keys=["avtemperatureIn","avtemperatureOut","avhumidityIn","avhumidityOut","avweigth0","avweigth1","avweigth2","avX","avY","avZ","avC02"]
 msg_to_TB_hist = {
-    1:{"values":{0:{"nsamples": 0,"avtemperatureIn": 0,"avtemperatureOut": 0,"avhumidityIn": 0,"avhumidityOut": 0,"avweigth0": 0,"avweigth1": 0,"avweigth2": 0,"avX": 0,"avY": 0,"avZ": 0,"avC02": 0}},
+    1:{"values":{0:{"ts":0 ,"nsamples": 0,"avtemperatureIn": 0,"avtemperatureOut": 0,"avhumidityIn": 0,"avhumidityOut": 0,"avweigth0": 0,"avweigth1": 0,"avweigth2": 0,"avX": 0,"avY": 0,"avZ": 0,"avC02": 0}},
             "notifications":{0:{"ts": 0,"alert_temp":"","alert_hum":"","weigth0": "ok","weigth1": "ok","weigth2": "ok","accel_alert":"","C02_alert":""}}},
-    2:{"values":{0:{"nsamples": 0,"avtemperatureIn": 0,"avtemperatureOut": 0,"avhumidityIn": 0,"avhumidityOut": 0,"avweigth0": 0,"avweigth1": 0,"avweigth2": 0,"avweigth3": 0,"avweigth4": 0,"avweigth4": 0,"avX": 0,"avY": 0,"avZ": 0,"avC02": 0}},
+    2:{"values":{0:{"ts":0 ,"nsamples": 0,"avtemperatureIn": 0,"avtemperatureOut": 0,"avhumidityIn": 0,"avhumidityOut": 0,"avweigth0": 0,"avweigth1": 0,"avweigth2": 0,"avweigth3": 0,"avweigth4": 0,"avweigth4": 0,"avX": 0,"avY": 0,"avZ": 0,"avC02": 0}},
             "notifications":{0:{"ts": 0,"alert_temp":"","alert_hum":"","weigth0": "ok","weigth1": "ok","weigth2": "ok","accel_alert":"","C02_alert":""}}},
-    3:{"values":{0:{"nsamples": 0,"avtemperatureIn": 0,"avtemperatureOut": 0,"avhumidityIn": 0,"avhumidityOut": 0,"avweigth0": 0,"avweigth1": 0,"avweigth2": 0,"avweigth3": 0,"avweigth4": 0,"avweigth4": 0,"avX": 0,"avY": 0,"avZ": 0,"avC02": 0}},
+    3:{"values":{0:{"ts":0 ,"nsamples": 0,"avtemperatureIn": 0,"avtemperatureOut": 0,"avhumidityIn": 0,"avhumidityOut": 0,"avweigth0": 0,"avweigth1": 0,"avweigth2": 0,"avweigth3": 0,"avweigth4": 0,"avweigth4": 0,"avX": 0,"avY": 0,"avZ": 0,"avC02": 0}},
             "notifications":{0:{"ts": 0,"alert_temp":"","alert_hum":"","weigth0": "ok","weigth1": "ok","weigth2": "ok","accel_alert":"","C02_alert":""}}}
     }
 value_TB={1:0,2:0,3:0}
@@ -45,11 +45,11 @@ mosquitto_broker = "127.0.0.1"
 values_hive_keys=["temperatureIn","temperatureOut","humidityIn","humidityOut","weigth0","weigth1","weigth2","X","Y","Z","C02"]
 msg_from_hive_hist = {
     1:{"values":{0:{"ts": 0,"temperatureIn": 0,"temperatureOut": 0,"humidityIn": 0,"humidityOut": 0,"weigth0": 0,"weigth1": 0,"weigth2": 0,"X": 0,"Y": 0,"Z": 0,"C02": 0}},
-            "notifications":{0:{"ts": 0,"alert_temp":"","alert_hum":"","weigth0": "ok","weigth1": "ok","weigth2": "ok","accel_alert":"","C02_alert":""}}},
+        "notifications":{0:{"ts": 0,"alert_temp":"","alert_hum":"","weigth0": "ok","weigth1": "ok","weigth2": "ok","accel_alert":"","C02_alert":""}}},
     2:{"values":{0:{"ts": 0,"temperatureIn": 0,"temperatureOut": 0,"humidityIn": 0,"humidityOut": 0,"weigth0": 0,"weigth1": 0,"weigth2": 0,"X": 0,"Y": 0,"Z": 0,"C02": 0}},
-            "notifications":{0:{"ts": 0,"alert_temp":"","alert_hum":"","weigth0": "ok","weigth1": "ok","weigth2": "ok","accel_alert":"","C02_alert":""}}},
+        "notifications":{0:{"ts": 0,"alert_temp":"","alert_hum":"","weigth0": "ok","weigth1": "ok","weigth2": "ok","accel_alert":"","C02_alert":""}}},
     3:{"values":{0:{"ts": 0,"temperatureIn": 0,"temperatureOut": 0,"humidityIn": 0,"humidityOut": 0,"weigth0": 0,"weigth1": 0,"weigth2": 0,"X": 0,"Y": 0,"Z": 0,"C02": 0}},
-            "notifications":{0:{"ts": 0,"alert_temp":"","alert_hum":"","weigth0": "ok","weigth1": "ok","weigth2": "ok","accel_alert":"","C02_alert":""}}}
+        "notifications":{0:{"ts": 0,"alert_temp":"","alert_hum":"","weigth0": "ok","weigth1": "ok","weigth2": "ok","accel_alert":"","C02_alert":""}}}
     }
 value_hive={1:0,2:0,3:0}
 notif_hive={1:0,2:0,3:0}
@@ -74,40 +74,38 @@ def on_message(client, userdata, msg):
     msg_dict=json.loads(msg.payload.decode('UTF-8'))
     global value_hive
     if "hive/1" in msg.topic:
-        print("hive1")
+        #print("hive1")
         if msg.topic == "hive/1/alert":
             pass
         elif msg.topic == "hive/1/telemetry":
-            print("telemetry from hive1 -> "+str(value_hive[1]))
+            #print("telemetry from hive1 -> "+str(value_hive[1]))
             msg_from_hive_hist[1]["values"][value_hive[1]]={}
             msg_from_hive_hist[1]["values"][value_hive[1]]["ts"]=time.time()
             msg_from_hive_hist[1]["values"][value_hive[1]].update(msg_dict)
             #print(json.dumps(msg_from_hive_hist[1])+"\n")
             value_hive[1] +=1
     if "hive/2" in msg.topic:
-        print("hive2")
+        #print("hive2")
         if msg.topic == "hive/2/alert":
             pass
         elif msg.topic == "hive/2/telemetry":
-            print("telemetry from hive2 -> "+str(value_hive[2]))
+            #print("telemetry from hive2 -> "+str(value_hive[2]))
             msg_from_hive_hist[2]["values"][value_hive[2]]={}
             msg_from_hive_hist[2]["values"][value_hive[2]]["ts"]=time.time()
             msg_from_hive_hist[2]["values"][value_hive[2]].update(msg_dict)
             #print(json.dumps(msg_from_hive_hist["hive2"])+"\n")
             value_hive[2] +=1
     if "hive/3" in msg.topic:
-        print("hive3")
+        #print("hive3")
         if msg.topic == "hive/3/alert":
             pass
         elif msg.topic == "hive/3/telemetry":
-            print("telemetry from hive3 -> "+str(value_hive[3]))
+            #print("telemetry from hive3 -> "+str(value_hive[3]))
             msg_from_hive_hist[3]["values"][value_hive[3]]={}
             msg_from_hive_hist[3]["values"][value_hive[3]]["ts"]=time.time()
             msg_from_hive_hist[3]["values"][value_hive[3]].update(msg_dict)
             #print(json.dumps(msg_from_hive_hist["hive3"])+"\n")
             value_hive[3] +=1
-            print("sent")
-
 
 def on_disconnect(client, userdata, rc):
     if rc != 0:
@@ -130,7 +128,7 @@ def calculate_avg(key,time_frame,ts,hive):
         if(msg_from_hive_hist[hive]["values"][i]["ts"])<(ts-time_frame):
             break
         result+=msg_from_hive_hist[hive]["values"][i][key]
-        print("i="+str(i)+" "+json.dumps(msg_from_hive_hist[hive]["values"][i]["ts"])+" "+json.dumps(msg_from_hive_hist[1]["values"][i][key]))
+        #print("i="+str(i)+" "+json.dumps(msg_from_hive_hist[hive]["values"][i]["ts"])+" "+json.dumps(msg_from_hive_hist[1]["values"][i][key]))
         ns+=1
     #print("key "+key+" ns "+str(ns)+" r "+str(result))
     if(ns>0):
@@ -139,21 +137,29 @@ def calculate_avg(key,time_frame,ts,hive):
         return 0
 
 def publish_avg(time_frame):
-    print("calculate")
     ts=time.time()
     global value_TB
     for i in range (1,4):
+        print("\n***** Hive "+str(i)+" *****")
         msg_to_TB_hist[i]["values"][value_TB[i]]={}
         key_ind=0
+        msg_to_TB_hist[i]["values"][value_TB[i]]["ts"]=ts
+        msg_to_TB_hist[i]["values"][value_TB[i]]["nsamples"]=calculate_ns(time_frame,ts,i)
         while key_ind < len(values_TB_keys):
             msg_to_TB_hist[i]["values"][value_TB[i]][values_TB_keys[key_ind]]=calculate_avg(values_hive_keys[key_ind],time_frame,ts,i)
             print("hive"+str(i)+" valuesTB "+str(value_TB[i])+" values_TB_key "+values_TB_keys[key_ind]+" value "+json.dumps(msg_to_TB_hist[i]["values"][value_TB[i]][values_TB_keys[key_ind]]))
-            #msg_to_TB_hist[i]["values"][value_TB[i]][values_TB_keys[key_ind]]=calculate_avg(values_hive_keys[key_ind],time_frame,ts,i)
-            #print("hive"+str(i)+" valuesTB "+str(value_TB[i])+" values_TB_key "+values_TB_keys[key_ind]+" value "+json.dumps(msg_to_TB_hist[i]["values"][value_TB[i]][values_TB_keys[key_ind]]))
             key_ind += 1
+        if i == 1:
+            device1.send_telemetry(msg_to_TB_hist[i]["values"][value_TB[i]])
+        elif i== 2:
+            device2.send_telemetry(msg_to_TB_hist[i]["values"][value_TB[i]])
+        elif i== 3:
+            device3.send_telemetry(msg_to_TB_hist[i]["values"][value_TB[i]])
+        print()
+    #print(json.dumps(msg_to_TB_hist[1]["values"][value_TB[1]]))
+    #device1.send_telemetry(msg_to_TB_hist[1]["values"][value_TB[1]])
     for i in range(1,4):
         value_TB[i]+=1
-        print(str(value_TB[i]))
 
 
 def periodic_avg(time_frame):
@@ -166,7 +172,7 @@ if __name__ == "__main__":
     try:
         start_hive_gt()
         TB_connect_all()
-        periodic_avg(20)
+        periodic_avg(60)
         while True:
             time.sleep(10)
     except KeyboardInterrupt:
